a55c9084fc1b7ee0b85b07bd1625b1f98e7ce920
ismael <joseismaelvargas@users.noreply.github.com>
2024-09-05 21:29:08 -0300

    "tarea Completada"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Helloword } from "./components/Helloword"
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <h1>hola mundo helloword.</h1>
+   <Helloword></Helloword>
     </>
   )
 }
---
+++ src/components/Helloword.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const Helloword=(props)=>{
+    const[mensaje,setmensaje]=useState(false)
+     const click=()=>{
+        if(setmensaje){
+            setmensaje(true)
+        }
+        
+     }
+    return(
+        <>
+        <h1>Hola. {props.mundo}{mensaje?" (Desde el Estado)":""}</h1>
+        <button className="" onClick={click}>Click</button>
+        </>
+    )
+}
